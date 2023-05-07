bb8df7b4ab8e1138bf0c5a00be00d92d3faf07d3
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-05-07 14:37:24 -0300

    "Updating"

---
+++ source/Worker/Delirio.Worker.EmailServer/Program.cs
@@ -0,0 +1,10 @@
+using Delirio.Worker.EmailServer;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
