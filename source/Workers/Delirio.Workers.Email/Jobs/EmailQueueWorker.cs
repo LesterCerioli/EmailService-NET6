1173d3ea109b1ea1cd230f1d7585f3be91a64e34
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-05-17 04:23:25 -0300

    feat(#560): Building Email Queue Worker

---
+++ source/Workers/Delirio.Workers.Email/Jobs/EmailQueueWorker.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delirio.Workers.Email.Jobs
+{
+    public class EmailQueueWorker : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly QueueClient _queueClient;
+
+        public EmailQueueWorker(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var connectionString = _configuration["QueueConnectionString"];
+            var queueName = _configuration["QueueName"];
+            _queueClient = new QueueClient(connectionString, queueName);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToke)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                QueueMessage message = await RetrieveMessageFromQueue();
+
+                if (message != null)
+                {
+
+                }
+            }
+        }
+    }
+}
