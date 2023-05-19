de9a11c1725c211849d79d9d8d7ac057ff919082
Aphonsus Smertz <aphonsus.smertz@outlook.com>
2023-05-19 18:45:04 +0000

    issue(#731): Building Data mappings

--- source/Infrastructure/Delirio.Infrastructure/Mappings/EFCore/OrderMap.cs
+++ source/Infrastructure/Delirio.Infrastructure/Mappings/EFCore/OrderMap.cs
@@ -8,7 +8,35 @@ namespace Delirio.Infrastructure.Mappings.EFCore
     {
         public void Configure(EntityTypeBuilder<Order> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(o => o.Id);
+
+            builder.Property(o => o.OrderBy)
+                .HasMaxLength(255);
+
+            builder.Property(o => o.TotalValue)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.HasOne(o => o.Customer)
+                .WithMany()
+                .HasForeignKey(o => o.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(o => o.Store)
+                .WithMany()
+                .HasForeignKey(o => o.StoreId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(o => o.Customers)
+                .WithOne()
+                .HasForeignKey("OrderId")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(o => o.Stores)
+                .WithOne()
+                .HasForeignKey("OrderId")
+                .OnDelete(DeleteBehavior.Cascade);
+
         }
     }
 }
