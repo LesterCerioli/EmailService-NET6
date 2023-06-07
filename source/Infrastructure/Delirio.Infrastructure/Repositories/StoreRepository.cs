f43c4b61fa54dfd157d238c903fe9d0b17cf304d
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-06-07 08:59:46 -0300

    refactor(#70): Builing Store Data repository

---
+++ source/Infrastructure/Delirio.Infrastructure/Repositories/StoreRepository.cs
@@ -0,0 +1,60 @@
+﻿using Delirio.Infrastructure.Context;
+using Delirio.Modules.Email.Domain.Contracts;
+using Delirio.Modules.Email.Domain.Models;
+using Delirio.Modules.Email.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delirio.Infrastructure.Repositories
+{
+    public class StoreRepository : IStoreRepository
+    {
+
+        protected readonly MailDbContext Db;
+        protected readonly DbSet<Store> DbSet;
+
+        public StoreRepository(MailDbContext context)
+        {
+            Db = context;
+            DbSet = context.Stores;
+        }
+
+        public async Task Add(Store store)
+        {
+            await Task.Run(() =>
+            {
+                DbSet.Add(store);
+                Db.SaveChanges();
+            });
+        }
+
+        public async Task<IEnumerable<Store>> GetAll()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task<Store> GetByCnpj(Cnpj cnpj)
+        {
+            return await DbSet.AsNoTracking().FirstOrDefaultAsync(c => c.Cnpj == cnpj);
+        }
+
+        public async Task<Store> GetByStoreName(string storeName)
+        {
+            return await DbSet.AsNoTracking().FirstOrDefaultAsync(c => c.StoreName == storeName);
+        }
+
+        public void Remove(Store store)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Store store)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
