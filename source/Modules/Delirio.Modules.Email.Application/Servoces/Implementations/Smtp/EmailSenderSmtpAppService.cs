2e1f928c2b415e3906f2dd9ff710ae2b778ec6e3
Aphonsus Smertz <aphonsus.smertz@outlook.com>
2023-06-09 00:26:53 +0000

    fet(#544): Building Email Services

---
+++ source/Modules/Delirio.Modules.Email.Application/Servoces/Implementations/Smtp/EmailSenderSmtpAppService.cs
@@ -0,0 +1,29 @@
+﻿using Delirio.Modules.Email.Application.Servoces.Contracts;
+using Delirio.Modules.Email.Domain.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delirio.Modules.Email.Application.Servoces.Implementations.Smtp
+{
+    public class EmailSenderSmtpAppService : IEmailSenderAppService
+    {
+        private readonly EmailConfig _emailConfig = new EmailConfig();
+
+        public EmailSenderSmtpAppService(IConfiguration config)
+        {
+            _emailConfig.SmtpServer = config.GetValue<string>("encomendasdelirio2023@outlook.com");
+            _emailConfig.SmtpUsername = config.GetValue<string>("encomendasdelirio2023@outlook.com");
+            _emailConfig.SmtpPassword = config.GetValue<string>("xaxa#2010x");
+            _emailConfig.SmtpPort = config.GetValue<int>("587");
+        }
+
+        public async Task SendEmailAsync(string recipient, string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/Modules/Delirio.Modules.Email.Domain/Models/EmailConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delirio.Modules.Email.Domain.Models
+{
+    public class EmailConfig
+    {
+        public string? SmtpServer { get; set; }
+
+        public int? SmtpPort { get; set; }
+
+        public string? SmtpUsername { get; set; }
+
+        public string? SmtpPassword { get; set; }
+    }
+}
